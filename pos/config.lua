14647844ad0aa945f808f55c93f572a48a3e34d8
Kristian Epps <kristian.epps@bbc.co.uk>
2014-11-28 13:27:23 +0000

    Added schema for a global config

---
+++ pos/config.lua
@@ -0,0 +1,63 @@
+global MODEM_SIDE = "back"
+
+global MONITOR_SIDE = "top"
+
+global SCREEN_TOP = 2
+
+global PORTS = {
+	"SHELF_ONE" = 1,
+	"SHELF_TWO" = 2,
+	"SHELF_THREE" = 3,
+	"SHELF_FOUR" = 4,
+	"POS" = 5,
+	"CASHIER" = 6
+}
+
+global SCREEN_TOP = 2
+
+global STOCK = {
+	"SHELF_ONE" = {
+		"id" = "wood",
+		"name" = "Wooden Armour",
+		"cost" = 2
+	},
+	"SHELF_TWO" = {
+		"id" = "iron",
+		"name" = "Iron Armour",
+		"cost" = 7
+	},
+	"SHELF_THREE" = {
+		"id" = "gold", 
+		"name" = "Golden Armour", 
+		"cost" = 25
+	},
+	"SHELF_FOUR" = {
+		"id" = "diamond", 
+		"name" = "Diamond Armour", 
+		"cost" = 200
+	}	
+}
+
+global EXCHANGE = {
+	17 = {
+		"id" = 17,
+		"name" = "Oak Wood",
+		"value" = (1/64)
+	},
+	264 = {
+		"id" = 264,
+		"name" = "Diamond",
+		"value" = 8
+	},
+	265 = {
+		"id" = 265,
+		"name" = "Iron Ingot",
+		"value" = (1/4)
+	},
+	266 = {
+		"id" = 266,
+		"name" = "Gold Ingot",
+		"value" = 1
+	},
+
+}
